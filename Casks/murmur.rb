f1fe1425d30591e729221e61fbc98bf660888cab
hydai <z54981220@gmail.com>
2026-09-08 17:13:12 +0800

    chore: update murmur to 0.2.16

--- Casks/murmur.rb
+++ Casks/murmur.rb
@@ -1,13 +1,13 @@
 cask "murmur" do
-  version "0.2.15"
-  sha256 "852ea09f1e8dc3c372835eefb41880621e94430c513070ed3aeb33a8200a866e"
+  version "0.2.16"
+  sha256 "dcec2e2e7fb5b41ae2c8c158298b53e790eeefc1e65537757d3a136e85a2103d"
 
   url "https://github.com/hydai/murmur/releases/download/v#{version}/Murmur_#{version}_aarch64.dmg"
   name "Murmur"
   desc "Privacy-first BYOK voice typing app"
   homepage "https://github.com/hydai/murmur"
 
-  depends_on :macos
+  depends_on macos: :tahoe
   depends_on arch: :arm64
 
   app "Murmur.app"
